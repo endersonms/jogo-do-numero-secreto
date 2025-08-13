3eecb9444fd0076df92cb427010024a73a3046e9
Enderson Marques <endersonmqsilva@gmail.com>
2025-08-13 13:36:35 -0300

    corrigindo instrução da tela inicial

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ function reiniciarJogo() {
 
 function mensagemInicial() {
     exibirTextoNaTela("h1", "Jogo do número secreto");
-    exibirTextoNaTela("p", "Escolha um número entre 1 e 5");
+    exibirTextoNaTela("p", "Escolha um número entre 1 e 50");
 }
 
 mensagemInicial();
